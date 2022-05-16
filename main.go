21d010d4b8c271a2ab334660b7aa252281dcdbcc
the-go-getter <vinay1020287@gmail.com>
2022-05-16 14:22:09 -0400

    Golang basics

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// x := 8
+	// y, z := 5, 13
+	// name := "Mercari"
+	employeeIds := make(map[string]int)
+	employeeIds["Vinay"] = 116
+	fmt.Println(employeeIds)
+	// fmt.Println(quote.Go())
+	// fmt.Println(x, y, z, name)
+
+}
