4efbd553b336cb4fb0afec92040ba4b3de59ce03
the-go-getter <vinay1020287@gmail.com>
2022-06-21 01:04:28 -0400

    Add token handler, logger, file server

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you've requested: %s with token: %s\n", r.URL.Path,
+		r.URL.Query().Get("token"))
+}
 
 func main() {
 	// x := 8
@@ -15,9 +21,10 @@ func main() {
 	// fmt.Println(quote.Go())
 	// fmt.Println(x, y, z, name)
 	// fmt.Println(rectangle.Area(2, 3))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+	http.HandleFunc("/", rootHandler)
 
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	log.Println("Web server has started")
 	http.ListenAndServe(":80", nil)
 }
